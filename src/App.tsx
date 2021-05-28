2eee041e5788d731daf5c6333b3593906f7df1db
Erick ZV <zv.erick@protonmail.com>
2021-05-28 13:10:27 -0500

    Get API data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import { FC } from "react";
+import axios from "axios";
+
+import Coin from "./apiModel";
 
 import "./App.css";
+
+const coinData = async () => {
+  try {
+    const res = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data: Coin[] = res.data;
+    return res.data ? data : "ERROR";
+  } catch (err) {
+    return "ERROR";
+  }
+};
 
 const App: FC = () => {
   return (
