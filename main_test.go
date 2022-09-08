2d1bb9943407cfd696c1df82ca8b1bb2013a7c4f
poximy <76846708+poximy@users.noreply.github.com>
2022-09-07 19:28:29 -0500

    feature: create main test file

---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	s := NewServer()
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	response := executeRequest(req, s)
+
+	if http.StatusNotFound != response.Code {
+		t.Errorf("Expected response code %d. Got %d\n", http.StatusNotFound, response.Code)
+	}
+
+	s.MountHandlers()
+	req, _ = http.NewRequest("GET", "/", nil)
+	response = executeRequest(req, s)
+
+	if http.StatusOK != response.Code {
+		t.Errorf("Expected response code %d. Got %d\n", http.StatusNotFound, response.Code)
+	}
+}
+
+func executeRequest(req *http.Request, s *Server) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	s.Router.ServeHTTP(rr, req)
+
+	return rr
+}
