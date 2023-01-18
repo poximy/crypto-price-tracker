f9f132be4cc4a617b02f82e44e4c65a10970a40c
Brayan-724 <bjqf0724@gmail.com>
2023-01-17 21:22:19 -0700

    [feature] Logs styling
    Add beautiful logs

--- main.go
+++ main.go
@@ -3,21 +3,33 @@ package main
 import (
 	"net/http"
 	"os"
+  "fmt"
+  "bytes"
+  "log"
+  "time"
+  "io"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/poximy/crypto-price-tracker/pages"
 )
 
+
 func main() {
 	s := NewServer()
 	s.MountMiddleware()
 	s.MountHandlers()
 
+  fmt.Println(pages.Green + pages.Bold + "\n    >> CRYPTO <<", pages.Reset)
+  fmt.Print(pages.Dim, ">" + pages.Reset)
+  fmt.Print(pages.Cyan + pages.Bold, "    port  " + pages.Reset)
+  fmt.Print(pages.Cyan + pages.Italic, port())
+  fmt.Print("\n\n\n", pages.Reset)
 	err := http.ListenAndServe(port(), s.Router)
 	if err != nil {
 		panic(err)
 	}
+
 }
 
 type Server struct {
@@ -25,7 +37,7 @@ type Server struct {
 }
 
 func (s *Server) MountMiddleware() {
-	s.Router.Use(middleware.Logger)
+  s.Router.Use(middleware.RequestLogger(&CustomLogFormatter{Logger: log.New(os.Stdout, "", log.LstdFlags)}))
 	s.Router.Use(middleware.Compress(5, "text/css", "application/javascript"))
 }
 
@@ -45,4 +57,85 @@ func port() string {
 		portNum = "8080" // Default port if not specified
 	}
 	return ":" + portNum
+}
+
+type CustomLogFormatter struct {
+  Logger middleware.LoggerInterface
+}
+
+func (l *CustomLogFormatter) NewLogEntry(r *http.Request) middleware.LogEntry {
+  useColor := true
+  entry := &customLogEntry {
+    CustomLogFormatter: l,
+    request:            r,
+    buf:                &bytes.Buffer{},
+    useColor:           useColor,
+  }
+
+
+	return entry
+}
+
+type customLogEntry struct {
+	*CustomLogFormatter
+	request  *http.Request
+	buf      *bytes.Buffer
+	useColor bool
+}
+
+func (l *customLogEntry) Write(status, bytes int, header http.Header, elapsed time.Duration, extra interface{}) {
+	cW(l.buf, l.useColor, pages.Magenta + pages.Bold, "%s ", l.request.Method)
+
+	switch {
+	case status < 200:
+		cW(l.buf, l.useColor, pages.Blue, "%03d", status)
+	case status < 300:
+		cW(l.buf, l.useColor, pages.Green, "%03d", status)
+	case status < 400:
+		cW(l.buf, l.useColor, pages.Cyan, "%03d", status)
+	case status < 500:
+		cW(l.buf, l.useColor, pages.Yellow, "%03d", status)
+	default:
+		cW(l.buf, l.useColor, pages.Red, "%03d", status)
+	}
+
+	cW(l.buf, l.useColor, pages.Cyan, " %s", l.request.RequestURI)
+	cW(l.buf, l.useColor, pages.Dim + pages.Yellow, "  %s ", ByteCountSI(bytes))
+
+	if elapsed < 500*time.Millisecond {
+		cW(l.buf, l.useColor, pages.Dim + pages.Green, "%s", elapsed)
+	} else if elapsed < 5*time.Second {
+		cW(l.buf, l.useColor, pages.Dim + pages.Yellow, "%s", elapsed)
+	} else {
+		cW(l.buf, l.useColor, pages.Dim + pages.Red, "%s", elapsed)
+	}
+
+	l.Logger.Print(l.buf.String())
+}
+
+func (l *customLogEntry) Panic(v interface{}, stack []byte) {
+	panic(v) //PrintPrettyStack(v)
+}
+
+// Use colors and format on writer
+func cW(w io.Writer, useColor bool, color string, s string, args ...interface{}) {
+  w.Write([]byte(color))
+  fmt.Fprintf(w, s, args...)
+  w.Write([]byte(pages.Reset))
+}
+
+// Format bytes size to B, kB, MB, TB, etc...
+// https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
+func ByteCountSI(b int) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
 }
---
+++ pages/color.go
@@ -0,0 +1,31 @@
+package pages
+
+import "fmt"
+
+var Reset  = "\033[0m"
+var Red    = "\033[31m"
+var Green  = "\033[32m"
+var Yellow = "\033[33m"
+var Blue   = "\033[34m"
+var Magenta = "\033[35m"
+var Cyan   = "\033[36m"
+var Gray   = "\033[37m"
+var White  = "\033[97m"
+
+var Bold = "\033[1m"
+var Dim = "\033[2m"
+var Italic = "\033[3m"
+
+var Clear = "\033[H\033[2J"
+
+func MakePrint(prefix, suffix string) (Print func(...any), Println func (...any)) {
+  return func(a ...any) {
+    fmt.Print(prefix)
+    fmt.Print(a...)
+    fmt.Print(suffix)
+  }, func(a ...any) {
+    fmt.Print(prefix)
+    fmt.Print(a...)
+    fmt.Println(suffix)
+  }
+}
