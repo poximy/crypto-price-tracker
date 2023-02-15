dfd0d10c8858727aaf19db77ebccdb962036dfa2
poximy <76846708+poximy@users.noreply.github.com>
2023-02-14 18:31:18 -0600

    [chore] prettify js string

--- src/script.js
+++ src/script.js
@@ -5,7 +5,28 @@ socket.onerror = (err) => {
   console.error(err);
 };
 function renderCard(data) {
-  return `<div class="flex h-48 flex-col items-center justify-center gap-2 border border-neutral-200 bg-neutral-100 p-2" data-name=${data.name}><img src=${data.image} alt=crypto class="mx-auto aspect-square w-12 sm:w-16" loading=lazy><div class="flex w-64 justify-evenly gap-2 text-lg sm:w-48"><p class=truncate>${data.name}<p class="font-bold uppercase">${data.symbol}</div><div class="flex w-64 justify-evenly font-mono sm:w-48"><p>${data.current_price}$<p>${data.price_change_percentage_24h}%</div></div>`;
+  return `
+      <div
+        class="flex h-48 flex-col items-center justify-center gap-2 border
+        border-neutral-200 bg-neutral-100 p-2"
+        data-name=${data.name}"
+      >
+        <img
+          src=${data.image}
+          alt="crypto"
+          class="mx-auto aspect-square w-12 sm:w-16"
+          loading="lazy"
+        />
+        <div class="flex w-64 justify-evenly gap-2 text-lg sm:w-48">
+          <p class="truncate">${data.name}</p>
+          <p class="font-bold uppercase">${data.symbol}</p>
+        </div>
+        <div class="flex w-64 justify-evenly font-mono sm:w-48">
+          <p>${data.current_price}$</p>
+          <p>${data.price_change_percentage_24h}%</p>
+        </div>
+      </div>
+  `;
 }
 window.addEventListener('DOMContentLoaded', () => {
   /** @type {HTMLDivElement} */
